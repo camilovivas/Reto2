7aa5693033825e99e59038d05cd22eaf30cb7ee7
camilovivas <camilo-152000@hotmail.com>
2021-11-24 11:01:54 -0500

    some changes

--- app/src/main/java/com/example/reto2/ListPokemon.java
+++ app/src/main/java/com/example/reto2/ListPokemon.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
