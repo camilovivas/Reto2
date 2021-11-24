7fb805d145b6c900fd8bbd2b27dd5c4a479dec3d
camilovivas <camilo-152000@hotmail.com>
2021-11-24 10:07:57 -0500

    login entrenador

--- app/src/main/java/com/example/reto2/ListPokemon.java
+++ app/src/main/java/com/example/reto2/ListPokemon.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 import model.Entrenador;
 
@@ -27,8 +30,8 @@ public class ListPokemon extends AppCompatActivity implements View.OnClickListen
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_pokemon);
-        entrenador = new Entrenador(getIntent().getExtras().getString("entrenador"));
         db = FirebaseFirestore.getInstance();
+        entrenador = entrenadorExist(getIntent().getExtras().getString("entrenador"));
         atrapar = findViewById(R.id.atrapar);
         buscar = findViewById(R.id.buscar);
         buscarBtn = findViewById(R.id.buscarBtn);
@@ -55,5 +58,26 @@ public class ListPokemon extends AppCompatActivity implements View.OnClickListen
     public void registrarEntrenador(Entrenador entrenador) {
         db.collection("entrenadores").document(entrenador.getNombre()).set(entrenador);
 
+    }
+
+    public Entrenador entrenadorExist(String name){
+        final Entrenador[] toReturn = {null};
+        Query query = db.collection("entrenadores").whereEqualTo("nombre", name);
+        query.get().addOnCompleteListener(
+                task -> {
+
+                    if(!task.getResult().isEmpty()){
+                        DocumentSnapshot ds = task.getResult().getDocuments().get(0);
+                        toReturn[0] = ds.toObject(Entrenador.class);
+                    }
+                    else{
+                        Entrenador n = new Entrenador(name);
+                        registrarEntrenador(n);
+                        toReturn[0] = n;
+                        Toast.makeText(this, "Has sido registrado, Bienvenido",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+        return toReturn[0];
     }
 }
